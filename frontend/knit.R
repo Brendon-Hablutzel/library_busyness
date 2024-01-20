130ee6fd90626c423e98680e15208b094b4dd6a4
Brendon-Hablutzel <booklvrbren@gmail.com>
2024-01-20 01:06:43 -0500

    move database access logic into knit.R to avoid environment errors

--- frontend/knit.R
+++ frontend/knit.R
@@ -1,3 +1,32 @@
 library(rmarkdown)
+library(dotenv)
+library(DBI)
+library(RMySQL)
 
-rmarkdown::render("libraries.Rmd", "html_document")
+# file paths are prepended with frontend because this file
+# should be run from the root directory of the project
+source("frontend/helpers.R")
+
+db <- dbConnect(RMySQL::MySQL(),
+  dbname = "busyness",
+  host = Sys.getenv("DB_HOST"),
+  user = Sys.getenv("DB_USER"),
+  password = Sys.getenv("DB_PASSWORD")
+)
+
+hill_data <- dbReadTable(db, "hill")
+hill_data <- entries_since_n_hrs_ago(
+  hill_data, to_datetime(hill_data$record_datetime), 24 * 7 * 5
+)
+
+hunt_data <- dbReadTable(db, "hunt")
+hunt_data <- entries_since_n_hrs_ago(
+  hunt_data, to_datetime(hunt_data$record_datetime), 24 * 7 * 5
+)
+
+dbDisconnect(db)
+
+rmarkdown::render(
+  "frontend/libraries.Rmd", "html_document",
+  params = list(hill_data = hill_data, hunt_data = hunt_data)
+)
