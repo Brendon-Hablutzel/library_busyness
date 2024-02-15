97d56f1da3d6b63632c4f646bb02e6cb7de286e6
Brendon Hablutzel <booklvrbren@gmail.com>
2024-02-15 14:46:43 -0500

    add validation for running scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-python3 $1
+PYTHON_CMD="python3"
+
+if [ "${1}" = ""  ]; then
+	echo "ERROR: an option (export or import) is required as the first argument"
+	exit 1
+fi
+
+if [ "${1}" = "import"  ]; then
+	echo "Importing from data/import/..."
+	$PYTHON_CMD import.py
+	exit 0
+fi
+
+if [ "${1}" = "export"  ]; then
+	echo "Exporting to data/export/..."
+	$PYTHON_CMD export.py
+	exit 0
+fi
+
+echo "ERROR: invalid option, expected 'export' or 'import'"
+exit 1
